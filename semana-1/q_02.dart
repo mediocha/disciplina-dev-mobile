bf76e92585ef8fe9ae233879577feb5644f6385f
mediocha <dr.ant.madson@gmail.com>
2021-08-19 19:16:54 -0300

    finalizando questões e criando questão 6.

--- semana-1/q_02.dart
+++ semana-1/q_02.dart
@@ -1,26 +1,33 @@
+import 'dart:io';
+
 /**
 Construa um programa pra ler e exibir uma matriz de inteiros.
-
--- não finalizada
-
  */
 
 
-import 'dart:io';
 void main() {
-  
-List<List<int>> matrix = new List<List<int>>();
-  for (var i = 0; i < 10; i++) {
-    List<int> list = new List<int>();
 
-    for (var j = 0; j < 10; j++) {
-      list.add(j);
+
+  List<List<int>> matriz = [];        // definindo a matriz - conjunto de listas
+
+  int pos;
+
+  print('Defina o tamanho da matriz: ');
+  int tam = int.parse(stdin.readLineSync()!);
+
+  for (var i = 0; i < tam; i++) {
+    
+    List<int> list = [];              // instanciando a lista
+
+    for (var j = 0; j < tam; j++) {
+      print('Posicao [${i+1}][${j+1}]:');
+      pos = int.parse(stdin.readLineSync()!);
+      list.add(pos);                  // leitura das posições
     }
 
-    matrix.add(list);
+    matriz.add(list);                 // adicionando a lista na matriz
   }
 
-  print(matrix);
-  print(matrix[2][4]);
+  print(matriz);
   
 }
